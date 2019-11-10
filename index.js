959254d2902fcec7b66c84c4ad2dbb34853fc02c
Andrew Sadowski <andrew.sadowski@developmentnow.com>
2019-11-09 23:01:53 -0800

    Created new index.js without using express

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-const express = require('express')
-const cors = require('cors')
-const graphqlHTTP = require('express-graphql')
-const gql = require('graphql-tag')
-// const { buildASTSchema } = require('graphql')
-const uuid = require('uuid/v4')
-const {schema} = require('./schema')
+const {graphql, buildSchema} = require('graphql')
 
-const app = express()
-app.use(cors())
 
-app.use('/graphql', graphqlHTTP({ schema, rootValue }))
 
-const port = process.env.PORT || 4000
-app.listen(port)
-console.log(`Running a GraphQL API server at localhost:${port}/graphql`)
+const schema = buildSchema(`
+  type Query {
+    foo: String
+  }
+  type Schema {
+    query: Query
+  }
+`)
